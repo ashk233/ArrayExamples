1b6d80c61630d52654bce0d767a7d9bfb056fdfe
ashk233 <43892820+ashk233@users.noreply.github.com>
2018-10-09 12:21:19 -0700

    Task 2 finished

--- Main.java
+++ Main.java
@@ -31,15 +31,31 @@ class Main
          */
         System.out.println("*** PRINTING ar1 *");
         int[] ar1 = new int[n];
+        //declare   allocates
         
         for (int i=0; i<n; i++)
-            ar1[i]=i;
+            ar1[i]=n-i;
+            //index value
+            
+        for (int i=0; i<n; i++)
+            System.out.println("ar1["+i+"] = "+ar1[i]);
+            //                  ar1[  i  ] =   i
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
+        System.out.println("** ar2 **");
+        int[] ar2 = new int[ar1.length];
         
+        // This clones ar1[] into ar2[]
+        // int[] ar2 = ar1.clone();
+        
+        for (int i=0; i<ar2.length; i++)
+            ar2[i]=ar1[i];
+        
+        for (int i=0; i<n; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
